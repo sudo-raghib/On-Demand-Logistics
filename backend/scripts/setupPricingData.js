5487d2e936f46025fd39a6f74b6af7fd512a52a8
sudo-raghib <raghibh2004@gmail.com>
2024-11-09 15:05:29 +0530

    price factor db fix

--- backend/scripts/setupPricingData.js
+++ backend/scripts/setupPricingData.js
@@ -5,6 +5,7 @@ const setupPricingData = async () => {
   try {
     const connectionUrl=process.env.DB_URL||"mongodb://localhost:27017"
     await mongoose.connect(`${connectionUrl}/logistics`, {
+      authSource: 'admin', 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
